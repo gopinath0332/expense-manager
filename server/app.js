5c445672d5d44fa72c7a02a61bf8e0f85c5bf169
Gopinath <gopinath0332@outlook.com>
2021-06-25 23:56:17 +0530

    add mongo cluster

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const mongoose = require("mongoose");
-const url = "mongodb://192.168.1.5/CarsDB";
+const url = "mongodb+srv://Gopinath0332:gopi0332*@cluster0.hsutt.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 
 var app = express();
 mongoose.connect(url, {
